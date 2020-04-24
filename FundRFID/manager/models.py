fa5377f70ebe134d750e77a5052d97e4c8f0736c
Owen Leynes <leyneso1@tcnj.edu>
2020-04-24 16:07:31 -0400

    added class year field and fixed upload_to statement

--- FundRFID/manager/models.py
+++ FundRFID/manager/models.py
@@ -1,16 +1,31 @@
 from django.db import models
 
 # Create your models here.
-
-# stores client info and score
-class Member(models.Model):
-	name = models.CharField(max_length=300)
-	tag_UID = models.CharField(max_length=16, blank=True)
-	email = models.EmailField(max_length=254)
-	score = models.IntegerField(blank=True)
-	img = models.ImageField(upload_to=create_upload_url, blank=True)
-	PAWS_ID = models.CharField(max_length=6)
+CLASS_YEAR = [
+	('FR', 'Freshman'),
+	('SO', 'Sophomore'),
+	('JR', 'Junior'),
+	('SR', 'Senior'),
+	('SS', 'Super Senior'),
+	('GR', 'Graduate'),
+	('AL', 'Alumni')
+]
 
 
 def create_upload_url(instance, filename):
 	return 'usr/{0}'.format(filename)
+
+
+# stores client info and score
+class Member(models.Model):
+	name = models.CharField(max_length=300)
+	tag_UID = models.CharField(max_length=16, blank=True, verbose_name='Tag UID')
+	email = models.EmailField(max_length=254)
+	score = models.IntegerField(blank=True)
+	img = models.ImageField(upload_to=create_upload_url, blank=True, verbose_name='Member Image')
+	PAWS_ID = models.CharField(max_length=6, verbose_name='PAWS ID')
+	class_year = models.CharField(max_length=2, choices=CLASS_YEAR, default='FR')
+
+
+
+
