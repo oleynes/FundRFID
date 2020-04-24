4c88843ee3de4b69218184f6458c7a3bae6328c6
Owen Leynes <leyneso1@tcnj.edu>
2020-04-24 16:08:15 -0400

    added register view for public members to create account

--- FundRFID/manager/views.py
+++ FundRFID/manager/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .forms import MemberForm
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'GET':
+        form = MemberForm()
+        return render(request, context={'form':form}, template_name='register.html')
+    elif request.method == 'POST':
+        member = MemberForm(request.POST)
+        if member.is_valid():
+            member.save()
