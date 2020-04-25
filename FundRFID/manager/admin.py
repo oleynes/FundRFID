f9ff6ce02931be68929d19da33ecfba8ffc5968d
Owen Leynes <leyneso1@tcnj.edu>
2020-04-24 20:37:50 -0400

    cleaned up admin registration

--- FundRFID/manager/admin.py
+++ FundRFID/manager/admin.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Member
 
+
 class MemberAdmin(admin.ModelAdmin):
 	fieldsets = [
 		('Member Information', {'fields': ['name, email, img, PAWS_ID']}),
 		('Tag Info', {'fields': ['tag_UID']}),
 		('Score Info', {'fields': ['score']}),
 	]
+
 
 admin.site.register(Member, MemberAdmin)
