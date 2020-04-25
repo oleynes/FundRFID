a435a4f87b142c66853ae068731b6a92483b10d3
Owen Leynes <leyneso1@tcnj.edu>
2020-04-25 02:58:45 -0400

    added views to support new templates

--- FundRFID/home/views.py
+++ FundRFID/home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, template_name='index.html')
+
+
+def about(request):
+    return render(request, template_name='about.html')
+
+
+def events(request):
+    return render(request, template_name='events.html')
+
+
+def eboard(request):
+    return render(request, template_name='eboard.html')
