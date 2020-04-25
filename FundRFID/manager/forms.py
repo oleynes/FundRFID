d6d1e704764177e571331badc04b4bb19ee562e7
Owen Leynes <leyneso1@tcnj.edu>
2020-04-24 20:38:31 -0400

    created lookup form

--- FundRFID/manager/forms.py
+++ FundRFID/manager/forms.py
@@ -1,8 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Member
 
 
-class MemberForm(ModelForm):
+class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
         fields = ['name', 'email', 'PAWS_ID', 'class_year']
+
+
+class LookupForm(forms.Form):
+    paws_id = forms.CharField(label='PAWS ID', max_length=6)
