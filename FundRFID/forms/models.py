23a157e84fe60c042de40a0dfee937439de919e7
Owen Leynes <leyneso1@tcnj.edu>
2020-05-13 22:42:08 -0400

    added FormLink model

--- FundRFID/forms/models.py
+++ FundRFID/forms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FormLink(models.Model):
+    link = models.CharField(max_length=500)
+    title = models.CharField(max_length=300)
+    description = models.TextField()
