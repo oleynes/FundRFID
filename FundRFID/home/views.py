b02073864de52ef83c005614a146acde225de3d1
Owen Leynes <leyneso1@tcnj.edu>
2020-05-13 22:40:36 -0400

    updated views to pass correct context to template

--- FundRFID/home/views.py
+++ FundRFID/home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import EboardMember, Event, Announcement
 
 # Create your views here.
 def index(request):
-    return render(request, template_name='index.html')
+    announcements = Announcement.objects.all().order_by('post_date')
+    return render(request, template_name='index.html', context={'announcements': announcements})
 
 
 def about(request):
@@ -11,8 +12,19 @@ def about(request):
 
 
 def events(request):
-    return render(request, template_name='events.html')
+    all_events = Event.objects.all().order_by('dt')
+    return render(request, template_name='events.html', context={'events': all_events})
+
+
+def sort_members(collection):
+    ls = [None] * 10
+    for m in collection:
+        ls[int(m.position)] = m
+    return ls
 
 
 def eboard(request):
-    return render(request, template_name='eboard.html')
+    eboard_members = EboardMember.objects.all()
+    eboard_members = sort_members(eboard_members)
+
+    return render(request, template_name='eboard.html', context={'eboard': eboard_members})
