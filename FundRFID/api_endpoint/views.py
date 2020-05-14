73d2b8967c52275354ed1209b5a2e3cfe3ad0a7c
Owen Leynes <leyneso1@tcnj.edu>
2020-05-13 22:43:11 -0400

    added handler for accepting post data and updating models

--- FundRFID/api_endpoint/views.py
+++ FundRFID/api_endpoint/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from manager.models import Member
+from django.db.models import F
+from django.http import JsonResponse
+
 
 # Create your views here.
+def update_cli(request, tag_id, score):
+    member = Member.objects.get(tag_UID=tag_id)
+    member.score = F('score') + score
+    try:
+        member.save()
+        return JsonResponse({'status': True})
+    except:
+        return JsonResponse({'status': False})
