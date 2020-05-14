a1fec6f24d96de05e129e1eb01da05256dceabbf
Owen Leynes <leyneso1@tcnj.edu>
2020-05-13 22:41:48 -0400

    added admin model for FormLink

--- FundRFID/forms/admin.py
+++ FundRFID/forms/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import FormLink
 # Register your models here.
+
+
+class FormLinkAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Link', {'fields': ['link']}),
+        ('Info', {'fields': ['title', 'description']}),
+    ]
+
+
+admin.site.register(FormLink, FormLinkAdmin)
